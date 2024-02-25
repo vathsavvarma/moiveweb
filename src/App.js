973cad6070822b02d6354951cc6acca8c6103c15
nameisvathsav <64901246+vathsavvarma@users.noreply.github.com>
2024-02-25 15:29:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {HashRouter  as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home.js';
 import VideoPlayer from './VideoPlayer.js';
 
